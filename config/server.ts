8bfeb5b461383fd5674ae64d3867ab9110410401
NayMontes <montesnayleth.99@gmail.com>
2025-10-08 15:15:29 -0500

    update config server database

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,11 @@
+import { url } from 'inspector';
+
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+	host: env('HOST', '0.0.0.0'),
+	port: env.int('PORT', 1337),
+	app: {
+		keys: env.array('APP_KEYS'),
+	},
+
+	url: env('PUBLIC_URL', 'https://ecommerce-shoes-backend-1.onrender.com'),
 });
