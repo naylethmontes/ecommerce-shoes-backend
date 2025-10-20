db32b52f0e379518685487da38ecd7eb7baf55ac
NayMontes <montesnayleth.99@gmail.com>
2025-10-20 10:38:55 -0500

    index middleware

--- src/middlewares/trust-proxy.ts
+++
@@ -1,8 +0,0 @@
-import { Context } from 'koa';
-
-export default (_config: unknown, { strapi }) => {
-	return async (ctx: Context, next: () => Promise<void>) => {
-		ctx.request.app.proxy = true;
-		await next();
-	};
-};
